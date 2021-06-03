dd16057969c7e4e117f4cf4a7f05a7060672369b
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-03 08:50:09 -0700

    BUGFIX: removed unneeded html form + fixed >= in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-  $("#height").submit(function(event) {
-    event.preventDefault();
-    const height = parseInt(prompt("How tall are you in inches?")); 
+  const height = parseInt(prompt("How tall are you in inches?"));
 
-  if (height =< 54) {
+  if (height <= 54) {
     $('#too-short').show();
-  } else if (age >54 and age <72) {
+  } else if (54 < height < 72) {
     $('#medium').show();
   } else {
-    $('too-tall').show();
+    $('#too-tall').show();
   }
 });
